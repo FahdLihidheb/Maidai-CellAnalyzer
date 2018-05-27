923879e38727ea6ee3a875b98896169c96025be0
PC-Malek <PC-Malek@DESKTOP-2EJTO6A>
2018-05-27 05:49:06 +0100

    Haha

--- src/app/cell-analyze/cell-analyze.component.ts
+++ src/app/cell-analyze/cell-analyze.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileUploaderOptions } from 'ng2-file-upload';
+import { DevEnv } from '../shared/config';
+
 
 @Component({
   selector: 'app-cell-analyze',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CellAnalyzeComponent implements OnInit {
 
+
+
+  public uploader: FileUploader = new FileUploader({ url: DevEnv.SERVER + 'api/uploadToAWS', itemAlias: 'imagePath' });
+
+   result: any
   constructor() { }
 
   ngOnInit() {
+
+
+
+    this.uploader.onBeforeUploadItem = (item) => {
+      item.withCredentials = false;
+    };
+    
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      const responseJSON = JSON.parse(response);
+      if (responseJSON.err_code === 0) {
+        this.result=responseJSON.diagnosis.data;
+        console.log(responseJSON);
+      }
+      this.uploader.queue[0].remove();
+    };
+
   }
 
 }
